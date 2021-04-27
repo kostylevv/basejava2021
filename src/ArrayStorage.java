b8858380fb5904b62520b2d84ecdaacc0f365706
kostylevv <kostylev.v@gmail.com>
2021-04-27 14:54:07 +0300

    Homework 1 implemented

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,57 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < size(); i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     void save(Resume r) {
+        if (r != null) {
+            storage[size()] = r;
+            size++;
+        }
     }
 
     Resume get(String uuid) {
+        int index = getResumeIndex(uuid);
+        if (index != -1) return storage[index];
         return null;
     }
 
     void delete(String uuid) {
+        int index = getResumeIndex(uuid);
+        if (index > -1) {
+            for (int i = index; i < size(); i++) {
+                storage[i] = storage[i + 1];
+            }
+            size--;
+        }
     }
+
+    private int getResumeIndex(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) return i;
+        }
+        return -1;
+    }
+
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumes = new Resume[size()];
+        for (int i = 0; i < size(); i++) {
+            resumes[i] = storage[i];
+        }
+        return resumes;
     }
 
     int size() {
-        return 0;
+        return size;
     }
+
+    private int size = 0;
 }
