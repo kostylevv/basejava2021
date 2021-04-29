d803c5359ece3e608913fa6f9d80d35eb0f8b83b
kostylevv <kostylev.v@gmail.com>
2021-04-29 16:55:39 +0300

    excluded size(), minor refactoring

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,9 +3,10 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
         size = 0;
@@ -13,7 +14,7 @@ public class ArrayStorage {
 
     void save(Resume r) {
         if (r != null) {
-            storage[size()] = r;
+            storage[size] = r;
             size++;
         }
     }
@@ -27,7 +28,7 @@ public class ArrayStorage {
     void delete(String uuid) {
         int index = getResumeIndex(uuid);
         if (index > -1) {
-            for (int i = index; i < size(); i++) {
+            for (int i = index; i < size; i++) {
                 storage[i] = storage[i + 1];
             }
             size--;
@@ -35,27 +36,20 @@ public class ArrayStorage {
     }
 
     private int getResumeIndex(String uuid) {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) return i;
         }
         return -1;
     }
 
-
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] resumes = new Resume[size()];
-        for (int i = 0; i < size(); i++) {
+        Resume[] resumes = new Resume[size];
+        for (int i = 0; i < size; i++) {
             resumes[i] = storage[i];
         }
         return resumes;
     }
-
-    int size() {
-        return size;
-    }
-
-    private int size = 0;
 }
